7d32600563ee297705ee9d756920098acc8dbcc0
kfoltyniak <kfoltyniak@software.altkom.pl>
2023-02-09 12:46:38 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-mask-test';
+  inputControl = new FormControl('')
+
+  ngOnInit(): void {
+    this.inputControl.valueChanges.subscribe(value => {
+      console.log(value);
+    })
+  }
 }
